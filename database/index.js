89c1db9029a6af899c3299817c09a535b4d33e2f
godouxx <gaspardaudoux@gmail.com>
2023-11-09 08:39:09 +0100

    reset du projet

--- database/index.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-mongoose
-  .connect(
-    "mongodb+srv://tleray:rtrade@rtrade.aysseip.mongodb.net/Rtrade?retryWrites=true&w=majority"
-  )
-  .then(() => console.log("Connected to MongoDB"))
-  .catch((err) => console.log(err));
--- routes/api.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-
-const trades = require("./api.trades");
-
-router.use("/trades", trades);
-
-module.exports = router;
